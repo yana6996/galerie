a2cc0c85e3e080c376fad83acd7831ed8c89662b
yana6996 <tranquilizer69@gmail.com>
2019-05-26 00:25:56 +0900

    2019/5/26 update

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 $(function(){
+  // メインビジュアルのアニメーション
   $('.main-visual').hide().fadeIn(2000);
+
+  // ここからスライダー
   $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 3000,
+    arrows: true,
     asNavFor: '.thumb',
+    infinite: true,
   });
   $('.thumb').slick({
     asNavFor: '.slider',
     focusOnSelect: true,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    infinite: true,
+  });
+  $(window).scroll(function (){
+      $('.profile , .garally , .contact').each(function(){
+          var targetElement = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight + 200){
+              $(this).css('opacity','1');
+              $(this).css('transform','translateY(0)');
+          }
+      });
   });
 });
 
